1134321f7c97d63f8bc90e3bc0c9849a644d1f54
saman azadi <samanazadi1996@gmail.com>
2024-04-14 05:36:06 +0330

    Add project files.

---
+++ CleanBinAndObj/Commands/MyCommand.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace CleanBinAndObj
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await CleanBinAndObjAsync();
+            await ShowMessageBoxAsync();
+        }
+
+        private async Task CleanBinAndObjAsync()
+        {
+            var activeProject = await VS.Solutions.GetActiveProjectAsync();
+            if (activeProject != null)
+            {
+                CleanDirectory(Path.GetDirectoryName(activeProject.FullPath));
+            }
+            else
+            {
+                var currentSolution = await VS.Solutions.GetCurrentSolutionAsync();
+                CleanDirectory(Path.GetDirectoryName(currentSolution.FullPath));
+            }
+        }
+
+        private void CleanDirectory(string directoryPath)
+        {
+            var directories = Directory.GetDirectories(directoryPath, "*", SearchOption.AllDirectories);
+            var binAndObjDirectories = directories.Where(dir =>
+                Path.GetFileName(dir).Equals("obj", StringComparison.OrdinalIgnoreCase) ||
+                Path.GetFileName(dir).Equals("bin", StringComparison.OrdinalIgnoreCase)).ToArray();
+            foreach (var dir in binAndObjDirectories)
+            {
+                Directory.Delete(dir, true);
+            }
+        }
+
+        private async Task ShowMessageBoxAsync()
+        {
+            await VS.MessageBox.ShowAsync("Clean 'bin' and 'obj'", "The 'bin' and 'obj' directories have been cleaned successfully.",buttons:Microsoft.VisualStudio.Shell.Interop.OLEMSGBUTTON.OLEMSGBUTTON_OK);
+        }
+    }
+}
